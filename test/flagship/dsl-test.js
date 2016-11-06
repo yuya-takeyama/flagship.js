bd205d8a6ce0f4178590c4f1b62c0402d14569e8
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2016-11-06 18:15:35 +0900

    Add Dsl

---
+++ test/flagship/dsl-test.js
@@ -0,0 +1,58 @@
+'use strict';
+
+import assert from 'power-assert';
+import Dsl from '../../src/flagship/dsl';
+
+describe('Dsl', () => {
+  describe('#getFlagset', () => {
+    it('creates a flagset with specified key', () => {
+      const flagset = (new Dsl('foo', (feature) => {
+        // noop
+      })).getFlagset();
+    });
+  });
+
+  describe('#enable', () => {
+    context('without function', () => {
+      it('enables specified feature', () => {
+        const dsl = new Dsl('foo', (feature) => {
+          feature.enable('bar');
+        });
+
+        assert(dsl.getFlagset().enabled('bar'));
+      });
+    });
+
+    context('with function', () => {
+      context('and it returns true', () => {
+        it('enables specified feature', () => {
+          const dsl = new Dsl('foo', (feature) => {
+            feature.enable('bar', () => true);
+          });
+
+          assert(dsl.getFlagset().enabled('bar'));
+        });
+      });
+
+      context('and it returns false', () => {
+        it('disables specified feature', () => {
+          const dsl = new Dsl('foo', (feature) => {
+            feature.enable('bar', () => false);
+          });
+
+          assert(dsl.getFlagset().enabled('bar') === false);
+        });
+      });
+    });
+  });
+
+  describe('#disable', () => {
+    it('disables specified feature', () => {
+      const dsl = new Dsl('foo', (feature) => {
+        feature.disable('bar');
+      });
+
+      assert(dsl.getFlagset().enabled('bar') === false);
+    });
+  });
+});
